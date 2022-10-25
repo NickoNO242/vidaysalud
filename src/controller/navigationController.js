28ba906ad15ccbc6ff753d4b00c17d83ed4cdd19
NickoNO242 <nico568_97@hotmail.com>
2022-10-25 07:31:53 -0500

    Agregar logo

--- src/controller/navigationController.js
+++ src/controller/navigationController.js
@@ -12,11 +12,14 @@ const navigationController ={
     getPuzle4 : (req, res , next) => {
         res.render('../views/PUZLE4.ejs')
     },
-    getPuzle5 : (req, res , next) => {
-        res.render('../views/PUZLE5.ejs')
-    },
-    getPuzle6 : (req, res , next) => {
-        res.render('../views/PUZLE6.ejs')
+    // getPuzle5 : (req, res , next) => {
+    //     res.render('../views/PUZLE5.ejs')
+    // },
+    // getPuzle6 : (req, res , next) => {
+    //     res.render('../views/PUZLE6.ejs')
+    // },
+    getPuzzle5 : (req, res , next) => {
+        res.render('../views/JUEG_ahorcado_F_2.ejs')
     },
     getRegisterForm : (req, res , next) => {
         res.render('../views/registerForm.ejs')
@@ -27,6 +30,7 @@ const navigationController ={
     getTips : (req, res , next) => {
         res.render('../views/tips.ejs')
     }
+
 }
 
 module.exports =navigationController
